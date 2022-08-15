e35ae116fd2aeba2db41230f6d7ad91db2c63cf7
Adrian <4114812+adrianpip2000@users.noreply.github.com>
2022-08-15 14:49:35 +0200

    Better selectors + removed spacebar + fixed style

--- add-hotkeys.js
+++ add-hotkeys.js
@@ -1,5 +1,5 @@
 var KeyCodes = {
-    "Space": 32,  // Pause
+//    "Space": 32,  // Pause
     "Left": 37,   // Back 15 seconds
     "Right": 39   // Forward 15 seconds
 };
@@ -8,22 +8,27 @@ var original_onkeydown = document.body.onkeydown;
 
 document.body.onkeydown = function(e) {
     let cls_clk = (cls) => document.querySelector(cls).click();
+/*
     switch (e.keyCode) {
         case KeyCodes.Space:
             console.log("Toggling playback");
             cls_clk("div.r-6dt33c:nth-child(2) > div:nth-child(1) > div:nth-child(2), div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)")
         break;
+*/
         case KeyCodes.Left:
             console.log("Skipping backward");
-            cls_clk("div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)");
+            cls_clk('div[data-testid="SkipBackwardButton"]');
         break;
         case KeyCodes.Right:
             console.log("Skipping forward");
-            cls_clk("div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3)");
+            cls_clk('div[data-testid="SkipForwardButton"]');
         break;
     };
     return original_onkeydown(e);
 }
 
-// Testing if I can make a small stylistic change
-document.querySelector("div.r-13awgt0:nth-child(4) > div:nth-child(2)").style.height = "200px"
+// Small stylistic change (height of playback background gradient)
+function fixStyle() {
+    document.querySelector("div.r-13awgt0:nth-child(4) > div:nth-child(2)").style.height = "200px"
+}
+window.onload = fixStyle;
