e0a1c327238650facecf8bf1613ccf3a8532ae41
Adrian <4114812+adrianpip2000@users.noreply.github.com>
2022-08-15 14:03:17 +0200

    Added spacebar pause
    in order to see if that will override the annoying behavior of HBO Max where if you manually click on fwd/bwd buttons, subsequent spacebar pressing will activate those buttons

--- add-hotkeys.js
+++ add-hotkeys.js
@@ -1,4 +1,5 @@
 var KeyCodes = {
+    "Space": 32,  // Pause
     "Left": 37,   // Back 15 seconds
     "Right": 39   // Forward 15 seconds
 };
@@ -8,6 +9,10 @@ var original_onkeydown = document.body.onkeydown;
 document.body.onkeydown = function(e) {
     let cls_clk = (cls) => document.querySelector(cls).click();
     switch (e.keyCode) {
+        case KeyCodes.Space:
+            console.log("Toggling playback");
+            cls_clk("div.r-6dt33c:nth-child(2) > div:nth-child(1) > div:nth-child(2), div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)")
+        break;
         case KeyCodes.Left:
             console.log("Skipping backward");
             cls_clk("div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)");
