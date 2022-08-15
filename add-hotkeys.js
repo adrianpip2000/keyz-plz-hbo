9c4940f626b7150e69e39bb60f25f2762596b879
Adrian <4114812+adrianpip2000@users.noreply.github.com>
2022-08-15 15:40:44 +0200

    debugging

--- add-hotkeys.js
+++ add-hotkeys.js
@@ -8,14 +8,14 @@ var original_onkeydown = document.body.onkeydown;
 
 document.body.onkeydown = function(e) {
     let cls_clk = (cls) => document.querySelector(cls).click();
-/*
     switch (e.keyCode) {
+/*
         case KeyCodes.Space:
             console.log("Toggling playback");
             cls_clk("div.r-6dt33c:nth-child(2) > div:nth-child(1) > div:nth-child(2), div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)")
         break;
 */
-/* Selecting by attribute names (didn't work)
+//Selecting by attribute names
         case KeyCodes.Left:
             console.log("Skipping backward");
             cls_clk('div[data-testid="SkipBackwardButton"]');
@@ -24,7 +24,8 @@ document.body.onkeydown = function(e) {
             console.log("Skipping forward");
             cls_clk('div[data-testid="SkipForwardButton"]');
         break;
-*/
+
+/*
         case KeyCodes.Left:
             console.log("Skipping backward");
             cls_clk("div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)");
@@ -33,9 +34,10 @@ document.body.onkeydown = function(e) {
             console.log("Skipping forward");
             cls_clk("div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3)");
         break;
+*/
     };
     return original_onkeydown(e);
-}
+};
 /*
 // Small stylistic change (height of playback background gradient)
 var playbackBkg = document.querySelector("div.r-13awgt0:nth-child(4) > div:nth-child(2)");
