e705a20a9c4464d3f1b66712137fdeaf46ed8b33
Adrian <4114812+adrianpip2000@users.noreply.github.com>
2022-08-15 16:49:36 +0200

    final-ish half-working version
    No style changes, only fwd/bwd skipping arrow keys

--- add-hotkeys.js
+++ add-hotkeys.js
@@ -12,30 +12,31 @@ document.body.onkeydown = function(e) {
         case KeyCodes.Left:
             console.log("Skipping backward");
             cls_clk('div[data-testid="SkipBackwardButton"]');
+			//fixStyle();
         break;
         case KeyCodes.Right:
             console.log("Skipping forward");
             cls_clk('div[data-testid="SkipForwardButton"]');
+			//fixStyle();
         break;
         /*
         // Pause/play with spacebar
         case KeyCodes.Space:
             console.log("Toggling playback");
-            cls_clk("div.r-6dt33c:nth-child(2) > div:nth-child(1) > div:nth-child(2), div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)")
+            cls_clk("div.r-6dt33c:nth-child(2) > div:nth-child(1) > div:nth-child(2), div.r-1pz39u2:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)");
+			//fixStyle();
         break;
         */
     };
     return original_onkeydown(e);
 };
 
-
-// Small stylistic change (height of playback background gradient)
-var playbackBkg = document.querySelector("div.r-13awgt0:nth-child(4) > div:nth-child(2)");
+/*
+// Small stylistic change (height of playback background gradient) - doesn't work very well because it resets every time the playback bar disappears
 function fixStyle() {
-    playbackBkg.style.height = "200px";
-}
-window.onload = fixStyle;
-
+    document.querySelector("div.r-13awgt0:nth-child(4) > div:nth-child(2)").style.height = "200px";
+};
+*/
 
 /*
 Bakup of different selectors
